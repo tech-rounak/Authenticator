b01bde60a1fd30fede11dd2b66a7ea447abeb2f5
tech_rounak <tech.rounak12@gmail.com>
2022-06-26 21:51:41 +0530

    [db.js] Connected atlas db

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const  url = "mongodb://localhost:27017/Authenticator"
+const  url = "mongodb+srv://basic-authentifier:basic-authentifier@cluster0.zxlwj.mongodb.net/?retryWrites=true&w=majority"
+// const  url = "mongodb://localhost:27017/Authenticator"
 mongoose.connect(url,{
     useNewUrlParser:true,
     useUnifiedTopology:true
