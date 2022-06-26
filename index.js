541a4c1bcc5121daf35812483dd2f3ae24bb6046
tech_rounak <tech.rounak12@gmail.com>
2022-06-26 21:34:51 +0530

    [index.js] changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sessions = require('express-session');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(sessions({
     secret: "Secret Key",
     saveUninitialized:true,
